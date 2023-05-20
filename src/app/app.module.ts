e91de3004e08f9e890001846739b5fae40d054d0
NewGrafon <ultrafig73@gmail.com>
2023-05-20 19:52:02 +0400

    ку ыыы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ItemCardComponent } from './components/item-card/item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingCartComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ProfileComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule,
