90d99e532854b8288358be4f50527733d32770e8
NewGrafon <ultrafig73@gmail.com>
2023-06-09 20:39:23 +0300

    add popup system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ItemCardComponent } from './components/item-card/item-card.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import {FormsModule} from "@angular/forms";
 import {NgOptimizedImage} from "@angular/common";
+import { PopupSystemComponent } from './components/popup-system/popup-system.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import {NgOptimizedImage} from "@angular/common";
     ProfileComponent,
     ItemCardComponent,
     CatalogComponent,
-    FilterByHomeCategories
+    FilterByHomeCategories,
+    PopupSystemComponent,
   ],
   imports: [
     BrowserModule,
